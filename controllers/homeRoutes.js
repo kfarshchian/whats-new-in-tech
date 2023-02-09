5f43ca3a8bae8df3e154347b1bba071e7c1bdcb6
Kameron <kfarshchian@gmail.com>
2023-02-09 16:08:18 -0700

    fixing minor stuff. still need bulma and css fixed

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,10 +1,9 @@
 const router = require('express').Router();
-const { Blog, User, Comment } = require('../models'); //add comment her
+const { Blog, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all blogs and JOIN with user data
     const blogData = await Blog.findAll({
       include: [
         {
@@ -14,10 +13,7 @@ router.get('/', async (req, res) => {
       ],
     });
 
-    // Serialize data so the template can read it
     const blogs = blogData.map((blog) => blog.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
     res.render('homepage', { 
       blogs, 
       logged_in: req.session.logged_in 
@@ -45,7 +41,6 @@ router.get('/blog/:id', async (req, res) => {
 
     const blog = blogData.get({ plain: true });
    console.log(blog);
-   //console.log("LOGGED IN?",req.session.logged_in)
     res.render('blog', {
       ...blog,
       logged_in: req.session.logged_in
@@ -55,10 +50,8 @@ router.get('/blog/:id', async (req, res) => {
   }
 });
 
-// Use withAuth middleware to prevent access to route
 router.get('/profile', withAuth, async (req, res) => {
   try {
-    // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
       include: [{ model: Blog }],
@@ -76,7 +69,6 @@ router.get('/profile', withAuth, async (req, res) => {
 });
 
 router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
     res.redirect('/profile');
     return;
@@ -85,9 +77,8 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
-/// added this signup
+
 router.get('/signup', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
     res.redirect('/profile');
     return;
--- models/User.js
+++ models/User.js
@@ -16,17 +16,10 @@ User.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    // name: {
-    //   type: DataTypes.STRING,
-    //   allowNull: false,
-    // },
     username: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
-      // validate: {
-      //   isusername: true,
-      // },
     },
     password: {
       type: DataTypes.STRING,
